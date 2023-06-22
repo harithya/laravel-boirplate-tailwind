d3a175187dcc3f6a48994cee0b4dfeee9553a592
sansan061100 <sansan001106@gmail.com>
2023-06-22 14:52:50 +0700

    first commit

---
+++ app/Http/Livewire/UserTable.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Rappasoft\LaravelLivewireTables\Views\Column;
+use App\Models\User;
+
+class UserTable extends DataTableComponent
+{
+    protected $model = User::class;
+
+
+    public function configure(): void
+    {
+        $this->setPrimaryKey('id');
+
+        $this->setSearchStatus(true);
+
+        $this->setSortingEnabled();
+
+        $this->setPaginationStatus(true);
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make("Id", "id")
+                ->sortable(),
+            Column::make("Name", "name")
+                ->sortable(),
+            Column::make("Email", "email")
+                ->sortable(),
+            Column::make("Created at", "created_at")
+                ->sortable(),
+            Column::make("Updated at", "updated_at")
+                ->sortable(),
+        ];
+    }
+}
